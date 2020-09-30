e9a8cc7fa63823980fd4c478b739540fb2ab637a
Paul Garrett <paul.garrett@laingsburg.k12.mi.us>
2020-09-30 11:01:26 -0400

    Finished project

---
+++ Dog.java
@@ -0,0 +1,14 @@
+public class Dog{
+
+    String breed;
+    String size;
+    int age;
+    String color;
+
+    public Dog(String breed, String size, int age, String color){
+        this.breed = breed;
+        this.size = size;
+        this.age = age;
+        this.color = color;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main{
 
     public static void main(String... args){
-        
+        Dog maltese = new Dog("maltese", "small", 3, "white");
+
+        System.out.println(maltese.breed);
     }
 }
